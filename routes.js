5f243d9618aaa1b6eee68ba40d6dd01524edb6eb
Alex Sacatoro <alexsacatoro02@gmail.com>
2022-10-12 18:49:15 -0500

    Listar datos de base de datos

--- routes.js
+++ routes.js
@@ -3,4 +3,14 @@ const express = require('express')
 const routes = express.Router()
 
 
+routes.get('/api/listar',(req, res) =>{
+    req.getConectiones((err,conn)=>{
+        if(err) return res.send(err);
+        conn.query,'SELECT * FROM datos',(err,rows) =>{
+        if(err) res.send(err)
+        res.json(rows);
+        }
+    })
+})
+
 module.exports = routes;
